1514f629c71bc9ed6bec4ed97a71a78293df1701
Legend1796 <m_igor97@mail.ru>
2022-03-27 22:55:04 +0300

    feat: редакция профиля под ключ

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,33 @@
-let editbtn = document.querySelector('.profile__editbtn');
-let editWindow = document.querySelector('.profile');
-
-
-
-function openEditWindow() {
-  editWindow.insertAdjacentHTML('afterbegin ', `
-        <div class="song">
-          <h4 class="song__artist">Кино</h4>
-          <p class="song__title">Дерево</p>
-          <button class="song__like"></button>
-        </div>
-  `);
+let editopen = document.querySelector('.profile__editbtn');
+let formElement = document.querySelector('.popup');
+let editclose = document.querySelector('.popup__close');
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__about');
+let username = document.querySelector('.profile__name');
+let about = document.querySelector('.profile__job');
+let save = formElement.querySelector('.popup__saveButton');
+function addText() {
+  nameInput.value = username.textContent;
+  jobInput.value = about.textContent;
 }
-
-editbtn.addEventListener('click', openEditWindow);
-
+function saveChanges() {
+  username.textContent = nameInput.value;
+  about.textContent = jobInput.value;
+  closePopup();
+  username.value = '';
+  about.value = '';
+}
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+}
+function openPopup() {
+  formElement.classList.add('popup_opened')
+  addText()
+}
+function closePopup() {
+  formElement.classList.remove('popup_opened')
+}
+editopen.addEventListener('click', openPopup);
+editclose.addEventListener('click', closePopup);
+save.addEventListener('click', saveChanges);
+formElement.addEventListener('submit', formSubmitHandler);
