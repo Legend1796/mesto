8f4d23306755ee7348e047053e003c6241bd23ba
Legend1796 <m_igor97@mail.ru>
2022-03-30 23:11:31 +0300

    fix: работа не принята доработка

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,28 @@
-let editopen = document.querySelector('.profile__editbtn');
+let editopen = document.querySelector('.profile__edit-btn');
 let formElement = document.querySelector('.popup');
 let editclose = document.querySelector('.popup__close');
 let nameInput = formElement.querySelector('.popup__name');
 let jobInput = formElement.querySelector('.popup__about');
 let username = document.querySelector('.profile__name');
 let about = document.querySelector('.profile__job');
-let save = formElement.querySelector('.popup__saveButton');
-let like = document.querySelector('.element__like');
-function addText() {
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  username.textContent = nameInput.value;
+  about.textContent = jobInput.value;
+  closePopup();
+}
+
+function openPopup() {
+  formElement.classList.add('popup_opened')
   nameInput.value = username.textContent;
   jobInput.value = about.textContent;
 }
-function saveChanges() {
-  username.textContent = nameInput.value;
-  about.textContent = jobInput.value;
-  username.value = '';
-  about.value = '';
-  closePopup();
-}
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-}
-function openPopup() {
-  formElement.classList.add('popup_opened')
-  addText()
-}
+
 function closePopup() {
   formElement.classList.remove('popup_opened')
 }
-function addLike() {
-  like.classList.add('element__like_active')
-}
-like.addEventListener('click', addLike);
+
 editopen.addEventListener('click', openPopup);
 editclose.addEventListener('click', closePopup);
-save.addEventListener('click', saveChanges);
 formElement.addEventListener('submit', formSubmitHandler);
