48e1357705cf6374dd984e1fca3aaa0b414ecc90
Igor Manukhov <m_igor97@mail.ru>
2022-04-22 22:04:18 +0300

    Валидация на оба попапа кроме кнопки

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,6 +1,6 @@
 // включение валидации вызовом enableValidation
 // все настройки передаются при вызове
-
+/*
 enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -9,3 +9,68 @@ enableValidation({
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 });
+
+*/
+
+// Функция, которая добавляет класс с ошибкой
+const showError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+// Функция, которая проверяет валидность поля
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideError(formElement, inputElement);
+  }
+};
+
+// Функция, которая передает элемент, который нужно проверить
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+}
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+}
+enableValidation();
+
+// Функция принимает массив полей и ищет хотя бы одно поле, которое не прошло валидацию
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+
+    return !inputElement.validity.valid;
+  })
+};
+
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__button_disabled');
+  } else {
+    buttonElement.classList.remove('popup__button_disabled');
+  }
+};
