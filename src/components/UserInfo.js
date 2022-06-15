4d77db87ad489615b68c53ab4f3d30b90ab152e7
Igor Manukhov <m_igor97@mail.ru>
2022-06-15 15:40:45 +0300

    add gh-pages

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -12,5 +12,16 @@ export class UserInfo {
   setUserInfo(userData) {
     this._userName.textContent = userData.name;
     this._userJob.textContent = userData.about;
+
+    fetch('https://mesto.nomoreparties.co/v1/cohort-43/cards', {
+      headers: {
+        authorization: 'f14e4888-1d0c-41bb-80a1-fc5f4ce8b4db'
+      }
+    })
+      .then(res => res.json())
+      .then((result) => {
+        console.log(result);
+      });
   }
 }
+
